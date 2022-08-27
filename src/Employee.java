89672bd08ef319c991bd6cab6b7e5bb0a51403f4
nawaz quazi <nawazquazi356@gmail,com>
2022-08-28 00:40:52 +0530

    created employe fil

---
+++ src/Employee.java
@@ -0,0 +1,86 @@
+public class Employee {
+    int salary;
+    String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+    class cellphone {
+        public void ring() {
+            System.out.println("ringing..");
+        }
+
+        public void vibrat() {
+            System.out.println("vibrating…");
+        }
+        public void call(){
+            System.out.println("calling....");
+        }
+    }
+    class Square{
+    public int  side(int side){
+        return side*side;
+    }
+    public int perimeter(int side){
+        return 4*side;
+    }
+    }
+class Tommy{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+    public void run(){
+        System.out.println("Running from the enemy");
+    }
+    public void fire(){
+        System.out.println("Firing on the enemy");
+    }
+}
+class circle{
+    public float area(int radius){
+        return 3.14f*radius*radius;
+    }
+}
+    class obj {
+
+        public static void main(String[] args) {
+
+//            circle ci = new circle();
+//            System.out.println(ci.area(34));
+
+//            Tommy player1 = new Tommy();
+//            player1.fire();
+//            player1.run();
+//            player1.hit();
+
+//            Square sq = new Square();
+//            System.out.println(sq.side(6));
+//            System.out.println(sq.perimeter(6));
+
+//            cellphone c = new cellphone();
+//            c.ring();
+//            c.vibrat();
+//            c.call();
+
+//        Employee em = new Employee();
+//        em.setName("nawaz");
+//        System.out.println(em.getName());
+//        em.setSalary(1234);
+//        System.out.println(em.getSalary());
+        }
+
+    }
+
