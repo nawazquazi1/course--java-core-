20aa964fd993d8ffd450e63e754439238aaab7b6
nawaz quazi <nawazquazi356@gmail,com>
2022-10-20 23:14:25 +0530

    classes in java

---
+++ src/org/oops/Classes.java
@@ -0,0 +1,51 @@
+package org.oops;
+
+public class Classes {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setSpeed(10);
+        System.out.println(car.getSpeed());
+    }
+}
+
+// car class
+class Car {
+    private String doors;
+    private String engine;
+    private String driver;
+    private int speed;
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+    public String getDoors() {
+        return doors;
+    }
+    public void setDoors(String doors) {
+        this.doors = doors;
+    }
+    public String getEngine() {
+        return engine;
+    }
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+    public String getDriver() {
+        return driver;
+    }
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+    public String run(){
+        if(doors.equals("closed") && engine.equals("on")
+                && driver.equals("seated") && speed >0	){
+            return "running";
+        }else
+            return "not running";
+    }
+
+
+}
